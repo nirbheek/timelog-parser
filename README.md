7200f3984d1a7a07d6db5a767f9d902e55f998da
Nirbheek Chauhan <nirbheek@centricular.com>
2020-02-01 11:23:09 +0530

    A timelog parser, format described in the README

---
+++ README.md
@@ -0,0 +1,83 @@
+## Requirements
+
+```sh
+$ pip3 install --user regex
+```
+
+## Description
+
+Timelog parser for a timelog text file in the following format:
+
+```
+== <monthname_year>
+
+date1: 2h30m of taskname-description, 15m of taskname-description2, 10m of taskname2-description3
+
+date2: 2h30m of taskname-description, 15m of taskname-description2, 10m of taskname2-description3
+
+...
+
+== <monthname2_year>
+
+...
+```
+
+* Report will aggregate everything inside the `<monthname_year>` block. Everything after `== ` is used as the heading for the report.
+* Each day's entry must begin with `<word>: ` where `<word>` is ignored. The remaining line consists of `<time> of <task>` timelog entries separated by `, `.
+* `<time>` is of the format `<number>h<number>m` or `<number>h` or `<number>m`
+* `<task>` is of the format `<taskname>-<description>` where:
+   - `<taskname>` must not contain any of: `<space>of<space>` or `,<space>` or `-`
+   - `<description>` must not contain any of: `<space>of<space>` or `,<space>`
+* By default, `<time>` is aggregated under `<taskname>`, but with the `--project-detail` option, aggregation will happen under `<taskname>-<description>`.
+* Invalid daily entries are ignored
+
+Example log (`test.txt`):
+
+```
+== Dec 2019
+
+1st: 4h of gnome-glib macOS CI, 1h of misc-IRC
+
+2nd: depression, no work
+
+3rd: 15m of misc-email, 2h50m of mozilla-cerbero-uwp-porting-work
+
+4th: sickness, no work
+
+5th: 30m of misc-email-and-IRC, 3h15m of mozilla-cerbero-openssl-variant at 1430
+```
+
+This will be aggregated as:
+
+```
+$ ./parse-timelog.py test.txt 0 -d
+Ignoring invalid/unknown entry 'depression'
+Ignoring invalid/unknown entry 'no work'
+Ignoring invalid/unknown entry 'sickness'
+Ignoring invalid/unknown entry 'no work'
+# Dec 2019
+Project              Hours      Cost
+mozilla               6h5m      0.00
+gnome                 4h        0.00
+misc                  1h45m     0.00
+Total:               11h50m     0.00
+```
+
+Change the `HOURLY_RATE` variable to calculate the `Cost` column correctly. To see more details, pass `--project-detail`:
+
+```
+$ ./parse-timelog.py test.txt 0 --project-detail
+Ignoring invalid/unknown entry 'depression'
+Ignoring invalid/unknown entry 'no work'
+Ignoring invalid/unknown entry 'sickness'
+Ignoring invalid/unknown entry 'no work'
+# Dec 2019
+Project              Hours      Cost
+gnome-glib macOS CI   4h        0.00
+mozilla-cerbero-openssl-variant  3h15m     0.00
+mozilla-cerbero-uwp-porting-work  2h50m     0.00
+misc-IRC              1h        0.00
+misc-email-and-IRC      30m     0.00
+misc-email              15m     0.00
+Total:               11h50m     0.00
+```
---
+++ parse-timelog.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+# We need the `regex` module instead of `re` for the branch reset feature
+import regex
+import argparse
+from pprint import pprint
+from decimal import Decimal
+
+SCRIPTDIR = os.path.normpath(os.path.dirname(__file__))
+ENTRY_TIME_RE = regex.compile(r'^(?|(?:([0-9.]+)h)(?:(\d+)m)?|(?:([0-9.]+)h)?(?:(\d+)m))$')
+HOURLY_RATE = 0
+
+def entry_time_to_minutes(t):
+    global ENTRY_TIME_RE
+    match = ENTRY_TIME_RE.match(t)
+    if not match:
+        raise AssertionError('Could not parse time entry {!r}'.format(t))
+    h, m = match.groups()
+    if not h and not m:
+        raise AssertionError('Both hours and minutes are zero for time entry {!r}'.format(t))
+    minutes = Decimal(m) if m is not None else 0
+    minutes += Decimal(h) * 60 if h is not None else 0
+    return minutes
+
+# Parse arguments
+parser = argparse.ArgumentParser(prog="parse-timelog")
+parser.add_argument('timelog_path', type=str, nargs='?', default=os.path.join(SCRIPTDIR, 'time-spent.txt'),
+                    help='Path to timelog file')
+parser.add_argument('month_idx', type=int, nargs='?', default=-1,
+                    help='Month index to parse')
+parser.add_argument('--project-detail', '-p', default=False, action='store_true',
+                    help='Summarize by project detail in the summary, not just by project')
+parser.add_argument('--decimal', '-d', default=False, action='store_true',
+                    help='Show hours in decimal instead of XXhYYm format')
+options = parser.parse_args()
+
+# Start parsing timelog
+timelog_f = open(options.timelog_path, 'r')
+
+# format:
+# [
+#   (month1, [line1, line2, line3, ..]),
+#   (month2, [line1, ...]),
+#   ...,
+# ]
+timelog_monthly = []
+month = None
+# Split timelog into monthwise entries
+for line in timelog_f:
+    # First, skip starting of file which is in a different format till we get
+    # a month entry, after which the format is more consistent
+    if not month and not line.startswith('== '):
+        continue
+    if line.startswith('== '):
+        month = line[3:-1]
+        month_entries = []
+        timelog_monthly.append((month, month_entries))
+        continue
+    if line[:-1]:
+        month_entries.append(line[:-1])
+
+month, days = timelog_monthly[options.month_idx - 1]
+# format:
+# {
+#   'project1': hours,
+#   'project2': hours,
+#   ...,
+# }
+proj_hours = {}
+# Aggregate per-category hours from this month's entries
+for day in days:
+    entries = day.split(': ', maxsplit=1)[1].split(', ')
+    for entry in entries:
+        try:
+            ret = entry.split(' of ', maxsplit=1)
+            if len(ret) != 2:
+                print('Ignoring invalid/unknown entry {!r}'.format(entry))
+                continue
+            entry_time, proj = ret
+        except ValueError:
+            print(entry)
+            raise
+        if ' at ' in proj:
+            proj = proj.split(' at ', maxsplit=1)[0]
+        if not options.project_detail:
+            proj = proj.split(sep='-', maxsplit=1)[0]
+        if proj not in proj_hours:
+            proj_hours[proj] = 0
+        proj_hours[proj] += entry_time_to_minutes(entry_time)
+
+def get_hhmmf(proj, hours, minutes):
+    timef = '{:>3}{:<3}'
+    if hours and minutes:
+        return timef.format('{}h'.format(hours), '{}m'.format(minutes))
+    elif hours:
+        return timef.format('{}h'.format(hours), '')
+    elif minutes:
+        return timef.format('', '{}m'.format(minutes))
+    raise AssertionError('No hours or minutes for {!r}'.format(proj))
+
+def get_decimalf(proj, hours, minutes):
+    timef = '{:>6.2f}'
+    hours = hours + (minutes / 60)
+    return timef.format(hours)
+
+def get_timef(proj, hours, minutes):
+    if options.decimal:
+        return get_decimalf(proj, hours, minutes)
+    return get_hhmmf(proj, hours, minutes)
+
+s = [(k, proj_hours[k]) for k in sorted(proj_hours, key=proj_hours.get, reverse=True)]
+print('# {}'.format(month))
+print('{:<20} {:^6} {:>8}'.format('Project', 'Hours', 'Cost'))
+total_minutes = 0
+total_cost = 0
+for proj, minutes in s:
+    total_minutes += minutes
+    hours = minutes // 60
+    minutes = (minutes - (hours * 60))
+    cost = HOURLY_RATE * round(hours + minutes/60, ndigits=2)
+    total_cost += cost
+    print('{:<20} {} {:>8}'.format(proj, get_timef(proj, hours, minutes), cost))
+hours = total_minutes // 60
+minutes = (total_minutes - (hours * 60))
+print('{:<20} {} {:>8}'.format('Total:', get_timef('total', hours, minutes), total_cost))
